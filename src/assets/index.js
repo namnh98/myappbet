5e751c20ea77eb30c66a3bf619d6d61a9b69ee52
namnh98 <namnh98@gmail.com>
2021-08-26 21:35:26 +0700

    update setup

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+export const images={
+    logo:require('../assets/images/logo.jpg'),
+    icon_key:require('../assets/images/key.png'),
+    icon_lock:require('../assets/images/lock_open.png'),
+    icon_person:require('../assets/images/person.png'),
+    icon_refresh:require('../assets/images/refresh.png')
+}
---
+++ src/styles/Color.js
@@ -0,0 +1,6 @@
+export const colors={
+    blue:'#1487bd',
+    orange:'#ff8930',
+    orange1:'#ff892f',
+    orange2:'#fe4b22'
+}
